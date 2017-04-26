449d97f0762768136be66b38dbb9a81da4aeae92
bysdxt <bysdxt@live.cn>
2017-04-26 12:50:36 +0800

    1

---
+++ clr_fs_lib/Library1.fs
@@ -0,0 +1,4 @@
+﻿namespace clr_fs_lib
+
+type Class1() = 
+    member this.X = "F#"
---
+++ clr_fs_main/Program.fs
@@ -0,0 +1,7 @@
+﻿// 在 http://fsharp.org 上了解有关 F# 的详细信息
+// 请参阅“F# 教程”项目以获取更多帮助。
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // 返回整数退出代码
